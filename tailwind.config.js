2ec500afec4c34f74872677beb54e22977e0ee1b
GovindSawant <code.govindsawant@gmail.com>
2025-04-29 20:48:31 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-// tailwind.config.js
-export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // 👈 This tells Tailwind where to look
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
