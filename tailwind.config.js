61577c4f91b9bfd794cc7fd2b49647e2d3647ffb
GovindSawant <code.govindsawant@gmail.com>
2025-04-29 20:38:36 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // 👈 This tells Tailwind where to look
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
